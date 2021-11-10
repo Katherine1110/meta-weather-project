aef62b70801383560125761e8d84640dadd1e1b3
Katherine1110 <k.mihalochkina@gmail.com>
2021-11-11 00:41:08 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './styles/header.scss';
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('.backdrop'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.backdrop.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
